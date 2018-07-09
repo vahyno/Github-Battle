abfa12586dd22593c7f3db9c3f56e505cf061c89
vahyno <vahyno@gmail.com>
2018-07-09 12:01:39 -0700

    initial commit

---
+++ src/components/Popular.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Popular extends Component {
+    state = {
+        selectedLanguage: 'All',
+    }
+
+    updateLanguage = (lang) => {
+        this.setState({
+            selectedLanguage: lang,
+        });
+    }
+
+    render() {
+        let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+        return (
+            <ul className='languages'>
+                {languages.map(lang=>{
+                    return (
+                        <li 
+                            style={lang === this.state.selectedLanguage ? {color : '#d0021b'}: null }
+                            onClick={this.updateLanguage.bind(null, lang) }
+                            key={lang}>
+                            { lang }
+                        </li>)
+                    })
+                }
+            </ul>
+        )
+    };
+}
+
+export default Popular;
